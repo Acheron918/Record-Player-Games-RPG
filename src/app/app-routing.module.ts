ccfb5d3b64da680ea08df69ddd2d3d234938332c
Acheron918 <97628296+Acheron918@users.noreply.github.com>
2022-02-07 12:14:59 -0700

    2nd

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'reviews',component:HomeComponent},
+  {path:'about',component:HomeComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
+import { AboutComponent } from './pages/about/about.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    ReviewsComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

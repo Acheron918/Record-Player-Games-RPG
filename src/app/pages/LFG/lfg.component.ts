9ca626d055c2a8cfec40742c93744fa844b7d16d
Marcus Backendorf <marcusbackendorf@gmail.com>
2022-04-04 11:58:44 -0600

    modal

--- src/app/pages/LFG/lfg.component.ts
+++ src/app/pages/LFG/lfg.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject  } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-lfg',
   templateUrl: './lfg.component.html',
   styleUrls: ['./lfg.component.css']
+ 
 })
 export class LfgComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _renderer2: Renderer2,
+    @Inject(DOCUMENT) private _document: Document
+  ) { }
 
+
+  
   ngOnInit(): void {
+    const script = this._renderer2.createElement('script');
+    script.type = 'text/javascript';
+    script.src = './assets/js/newgroup.js';
+    this._document.body.appendChild(script);
+  }
+  onShow() {
+    alert('Show button clicked!');
   }
 
 }
+
+
